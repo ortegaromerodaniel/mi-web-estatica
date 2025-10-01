0901b545f280e8fafa05653aed8254bc9ceb90ad
Ortega <ortega.romero.daniel@iescamas.es>
2025-10-01 12:50:13 +0200

    he cambiado la cabecera, el pie de pagina, una tabla, y los colores de la tabla

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById('toggleButton');
     const extraInfo = document.getElementById('extraInfo');
+	
+	const celdas = document.querySelectorAll('#miTabla td');
+
+	celdas.forEach(celda => {
+	  celda.addEventListener('mouseenter', () => {
+	    celda.style.backgroundColor = 'green';  // color al pasar el cursor
+	  });
+
+	  celda.addEventListener('mouseleave', () => {
+	    celda.style.backgroundColor = '';  // vuelve al color original
+	  });
+	});
 
     // Función para mostrar u ocultar la información adicional
     toggleButton.addEventListener('click', function() {
